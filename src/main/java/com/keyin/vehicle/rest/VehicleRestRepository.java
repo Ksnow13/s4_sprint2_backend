def98312c9e53bab5b13f580d732b13172791f74
Ksnow <107448069+Ksnow13@users.noreply.github.com>
2023-08-18 12:44:57 -0230

    finalize

--- src/main/java/com/keyin/vehicle/rest/VehicleRestRepository.java
+++ src/main/java/com/keyin/vehicle/rest/VehicleRestRepository.java
@@ -4,11 +4,14 @@ import com.keyin.vehicle.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "vehicle", path = "vehicle")
 public interface VehicleRestRepository extends JpaRepository<Vehicle, Long> {
+
 
     public List<Vehicle> findByBrand(@Param("brand")String brand);
     public List<Vehicle> findByModel(@Param("model")String model);
